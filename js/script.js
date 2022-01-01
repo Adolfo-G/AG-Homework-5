b8981e6c4ed49f2b628a39311d95ee916a636a11
Adolfo Gonzalez <agonzal177@gmail.com>
2022-01-01 15:12:53 -0500

    added meta tag to refresh page every 30 seconds in html , adjusted button padding in css, restructured files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 var now=moment();
 $("#currentDay").text(now.format("dddd, MMMM Do YYYY"));
 
-
 const hours=["9 AM","10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
 const milTime=[9,10,11,12,13,14,15,16,17]
 const tag=["0","1","2","3","4","5","6","7","8"]
